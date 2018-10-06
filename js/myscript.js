a45029d077621ce6f613c17ec8f05cd94178f06c
Mario Weiss <mobil@ing-weiss.com>
2018-10-07 00:49:25 +0200

    add actual genre, add no_js notice

--- js/myscript.js
+++ js/myscript.js
@@ -1,10 +1,11 @@
 var movies_arr = [];
 var genre_arr = [];
 var sort_arr = ["name", "year","likes"];
-var filter;
+var filter = "show_all";
+var filterClasses = ".";
 
 $.get("js/movies.json", function(data) {
-  fillcontent(data.movies);
+  fillContent(data.movies);
   movies_arr = data.movies;
 });
 
@@ -13,12 +14,26 @@ $(sort_arr).each( function(i){
 	$("#sort_desc_"+sort_arr[i]).click(function() {sortString(sort_arr[i], "desc")});
 })
 
-function fillcontent(movies) {
+function filterContent(genre){
+	filter = genre;
+	if (genre == "show_all") {
+		$(filterClasses).slideUp(50);
+		$(filterClasses).slideDown(500);
+		$("#currentGenre").html("all genres");
+	} else {
+		$(filterClasses).slideUp(50);
+		$("."+genre).slideDown(500);
+		$("#currentGenre").html(genre.replace(/_/g,' '));
+	}
+}
+
+function fillContent(movies) {
 //-----create content of page-----//
 	var content = ""
 	$(movies).each(function(i) {
 //-----handover the genre as class to the container, so we can filter it later on-----//
-		var genre_div = (movies[i].genre).join(" ");
+		var genre_div = (movies[i].genre).join("&-&").replace(/ /g,'_').replace(/&-&/g,' ');
+		console.log(genre_div)
 		content += "<div class='col-12 col-md-6 col-lg-6 col-xl-4 py-2 "+genre_div+"'>";
 		content += "		<div class='test bg-verydark p-2 d-flex rounded'>";
 		content += "			<img class='rounded' src='img/"+movies[i].img+"' alt='"+movies[i].name+"'>";
@@ -44,17 +59,25 @@ function fillcontent(movies) {
 			if($.inArray(value, genre_arr) === -1) genre_arr.push(value);
 		});
 	});
-	genre_arr.sort();
-	genre_arr.unshift("show_all");
+//-----if it is the first time we sort it, replace all spaces with _ and add show_all-----//
+	if (genre_arr[0] != "show_all") {
+		genre_arr.sort();
+		$(genre_arr).each(function(i,value){genre_arr[i] = value.replace(/ /g,'_')})
+		genre_arr.unshift("show_all");
+	}
 //-----add values für filtering-----//
-	var filterContent = ""
+	var filterValues = "";
+	filterClasses = "";
 	$(genre_arr).each(function(i, value){
-		filterContent += "<button id='filter_"+value+"' class='dropdown-item'>"+value.replace('_',' ')+"</button>"
+		filterValues += "<button id='filter_"+value+"' class='dropdown-item'>"+value.replace('_',' ')+"</button>"
+		filterClasses += "."+value+", ";
 	})
-	$("#genre_filter").html(filterContent);
+	$("#genre_filter").html(filterValues);
 	$(genre_arr).each(function(i, value){
-		$("#filter_"+value).click(function(){filter(value)});
+		$("#filter_"+value).click(function(){filterContent(value)});
 	});
+	filterClasses = filterClasses.slice(0, -2);
+	filterContent(filter);
 }
 
 function likeUp() {
@@ -65,8 +88,7 @@ function likeUp() {
 			tempId = "#"+tempId+" div h4";
 			$(tempId).text(movies_arr[i].likes);	
 		}
-	})
-	console.log(genre_arr)	
+	})	
 }
 
 function sortString(field, order) {
@@ -92,25 +114,5 @@ function sortString(field, order) {
 			return 0;
 		});
 	}
-	fillcontent(movies_arr);
+	fillContent(movies_arr);
 }
-
-function filter(genre){
-	if (genre == "show_all") {
-		$(genre_arr).each(function(i, value){
-			$("."+value).show(500);
-		})
-	} else {
-		$(genre_arr).each(function(i, value){
-			$("."+value).hide(500);
-		})
-		$("."+genre).show(500);
-	}
-}
-
-/* deprecated version but working!
-var msg = $.ajax({type: "GET", url: "js/movies.json", async: false}).responseText;
-console.log(msg)
-var result = JSON.parse(msg);
-console.log(result)
-*/
