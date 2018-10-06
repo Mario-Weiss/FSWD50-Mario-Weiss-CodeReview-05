99b4abe31a530b5421d03bd52c57854b550e53bf
Mario Weiss <mobil@ing-weiss.com>
2018-10-06 05:16:10 +0200

    include sorting

--- js/myscript.js
+++ js/myscript.js
@@ -1,10 +1,15 @@
 var movies_arr
+var sort_arr = ["name","overview","year","likes"];
 
 $.get("js/movies.json", function(data) {
   fillcontent(data.movies);
   movies_arr = data.movies;
 });
 
+$(sort_arr).each( function(i){
+	$("#sort_"+sort_arr[i]).click(function() {sortString(sort_arr[i], "asc")});
+	$("#sort_desc_"+sort_arr[i]).click(function() {sortString(sort_arr[i], "desc")});
+})
 
 function fillcontent(movies) {
 //-----create content of page-----//
@@ -39,10 +44,34 @@ function likeUp() {
 			tempId = "#"+tempId+" div h4";
 			$(tempId).text(movies_arr[i].likes);	
 		}
-	})
-	
+	})	
 }
 
+function sortString(field, order) {
+	if ((eval("movies_arr[0]."+field)).toFixed) {
+		movies_arr.sort(function(a, b) {
+		if (order == "desc") {
+			return eval("b."+field) - eval("a."+field);
+		} else {
+			return eval("a."+field) - eval("b."+field);
+		}
+	});
+	} else {
+		movies_arr.sort(function(a, b){
+			var x = eval("a."+field+".toLowerCase()");
+			var y = eval("b."+field+".toLowerCase()");
+			if (order == "desc") {
+				if (x > y) {return -1;}
+				if (x < y) {return 1;}
+			} else {
+				if (x < y) {return -1;}
+				if (x > y) {return 1;}
+			}
+			return 0;
+		});
+	}
+	fillcontent(movies_arr);
+}
 
 /* deprecated version but working!
 var msg = $.ajax({type: "GET", url: "js/movies.json", async: false}).responseText;
